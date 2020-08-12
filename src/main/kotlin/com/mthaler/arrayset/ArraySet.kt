f87973727a1617995a0ed8368a4a28a1d5dd2e18
Michael Thaler <michael.thaler@dlr.de>
2020-08-12 08:44:35 +0200

    Added ArraySet.kt.

---
+++ src/main/kotlin/com/mthaler/arrayset/ArraySet.kt
@@ -0,0 +1,22 @@
+package com.mthaler.arrayset
+
+abstract class ArraySet<E> : Collection<E> {
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun contains(element: E): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsAll(elements: Collection<E>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): Iterator<E> {
+        TODO("Not yet implemented")
+    }
+}
