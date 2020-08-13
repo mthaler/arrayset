1245f0d62b897a651c85770e62e576f93f01f8dd
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 07:36:50 +0200

    Updated Sorting.kt.

--- src/main/kotlin/com/mthaler/arrayset/Sorting.kt
+++ src/main/kotlin/com/mthaler/arrayset/Sorting.kt
@@ -1,24 +1,8 @@
 package com.mthaler.arrayset
 
-import java.util.Comparator
-
 object Sorting {
 
     object InsertionSort {
-
-        fun <T>sort(data: Array<T>, start: Int, end: Int, c: Comparator<T>) {
-            var i = start + 1
-            while (i < end) {
-                val item = data[i]
-                var hole = i
-                while(hole > start && c.compare(data[hole - 1], item) > 0) {
-                    data[hole] = data[hole - 1]
-                    hole -= 1
-                }
-                data[hole] = item
-                i += 1
-            }
-        }
 
         fun <T : Comparable<T>>sort(data: Array<T>, start: Int, end: Int) {
             var i = start + 1
@@ -32,6 +16,48 @@ object Sorting {
                 data[hole] = item
                 i += 1
             }
+        }
+    }
+
+    object QuickSort {
+        val limit: Int = 16
+
+        fun <T : Comparable<T>>qsort(data: Array<T>) {
+            qsort(data, 0, data.size - 1)
+        }
+
+        fun <T : Comparable<T>>qsort(data: Array<T>, left: Int, right: Int) {
+            if (right - left < limit)
+                InsertionSort.sort(data, left, right + 1)
+            else {
+                val pivot = left + (right - left) / 2
+                val next = partition(data, left, right, pivot)
+                qsort(data, left, next - 1)
+                qsort(data, next + 1, right)
+            }
+        }
+
+        fun <T : Comparable<T>>partition(data: Array<T>, left: Int, right: Int, pivot: Int): Int {
+            val value = data[pivot]
+
+            //swap(pivot, right)
+            var tmp = data[pivot]; data[pivot] = data[right]; data[right] = tmp
+
+            var store = left
+            var i = left
+
+            while (i < right) {
+                if (data[i].compareTo(value) < 0) {
+                    //swap(i, store)
+                    tmp = data[i]; data[i] = data[store]; data[store] = tmp
+                    store += 1
+                }
+                i += 1
+            }
+
+            //swap(store, right)
+            tmp = data[store]; data[store] = data[right]; data[right] = tmp
+            return store
         }
     }
 }
--- src/test/kotlin/com/mthaler/arrayset/SortingTest.kt
+++ src/test/kotlin/com/mthaler/arrayset/SortingTest.kt
@@ -6,26 +6,19 @@ import org.junit.jupiter.api.Test
 class SortingTest {
 
     @Test
-    fun insertionSortComparator() {
+    fun insertionSort() {
         val a = arrayOf(3, 2, 5, 4, 8, 12, 1)
-        Sorting.InsertionSort.sort(a, 0, a.size, object : Comparator<Int> {
-            override fun compare(x: Int, y: Int): Int {
-                if (x > y) {
-                    return 1
-                } else if (x < y) {
-                    return -1
-                } else {
-                    return 0
-                }
-            }
-        })
+        Sorting.InsertionSort.sort(a, 0, a.size)
         assertArrayEquals(arrayOf(1, 2, 3, 4, 5, 8, 12), a)
     }
 
     @Test
-    fun insertionSortComparable() {
-        val a = arrayOf(3, 2, 5, 4, 8, 12, 1)
-        Sorting.InsertionSort.sort(a, 0, a.size)
-        assertArrayEquals(arrayOf(1, 2, 3, 4, 5, 8, 12), a)
+    fun quickSort() {
+        val a0 = arrayOf(3, 2, 5, 4, 8, 12, 1)
+        Sorting.QuickSort.qsort(a0)
+        assertArrayEquals(arrayOf(1, 2, 3, 4, 5, 8, 12), a0)
+        val a1 = arrayOf(20, 1, 19, 2, 18, 3, 17, 4, 16, 5, 15, 6, 14, 7, 13, 8, 12, 9, 11, 10)
+        Sorting.QuickSort.qsort(a1)
+        assertArrayEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20), a1)
     }
 }
