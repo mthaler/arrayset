c16e746046ef2671f67d2a6779aa40c430dafbfb
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 13:48:38 +0200

    Added SetUtils.diff.

--- src/main/kotlin/com/mthaler/arrayset/SetUtils.kt
+++ src/main/kotlin/com/mthaler/arrayset/SetUtils.kt
@@ -10,6 +10,9 @@ object SetUtils {
     fun  <E : Comparable<E>>intersection(a: Array<E>, b: Array<E>): Array<E> =
         IntersectionMerge(a, b).result()
 
+    fun  <E : Comparable<E>> diff(a: Array<E>, b: Array<E>): Array<E> =
+        DiffMerge(a, b).result()
+
     class UnionMerge<E : Comparable<E>>(val a: Array<E>, val b: Array<E>) : BinaryMerge() {
         val r = ArrayUtils.newArray(a.size + b.size, a)
         var ri: Int = 0
@@ -54,6 +57,28 @@ object SetUtils {
             ri += 1
         }
         override fun fromA(a0: Int, a1: Int, bi: Int) {}
+
+        override fun fromB(ai: Int, b0: Int, b1: Int) {}
+
+        fun result(): Array<E> = ArrayUtils.resizeInPlace(r, ri)
+    }
+
+    class DiffMerge<E : Comparable<E>>(val a: Array<E>, val b: Array<E>) : BinaryMerge() {
+        val r = ArrayUtils.newArray(a.size, a)
+        var ri: Int = 0
+
+        init {
+            merge0(0, a.size, 0, b.size)
+        }
+
+        override fun compare(ai: Int, bi: Int) = a[ai].compareTo(b[bi])
+
+        override fun collision(ai: Int, bi: Int) {}
+
+        override fun fromA(a0: Int, a1: Int, bi: Int){
+            System.arraycopy(a, a0, r, ri, a1 - a0)
+            ri += a1 - a0
+        }
 
         override fun fromB(ai: Int, b0: Int, b1: Int) {}
 
