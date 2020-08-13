5b87e2c20014b0f524bc565bdc0e429c4a14e6a6
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 13:53:11 +0200

    Added SetUtils.xor.

--- src/main/kotlin/com/mthaler/arrayset/SetUtils.kt
+++ src/main/kotlin/com/mthaler/arrayset/SetUtils.kt
@@ -13,6 +13,9 @@ object SetUtils {
     fun  <E : Comparable<E>> diff(a: Array<E>, b: Array<E>): Array<E> =
         DiffMerge(a, b).result()
 
+    fun <E : Comparable<E>>xor(a: Array<E>, b: Array<E>): Array<E> =
+        XorMerge(a, b).result()
+
     class UnionMerge<E : Comparable<E>>(val a: Array<E>, val b: Array<E>) : BinaryMerge() {
         val r = ArrayUtils.newArray(a.size + b.size, a)
         var ri: Int = 0
@@ -75,12 +78,36 @@ object SetUtils {
 
         override fun collision(ai: Int, bi: Int) {}
 
-        override fun fromA(a0: Int, a1: Int, bi: Int){
+        override fun fromA(a0: Int, a1: Int, bi: Int) {
             System.arraycopy(a, a0, r, ri, a1 - a0)
             ri += a1 - a0
         }
 
         override fun fromB(ai: Int, b0: Int, b1: Int) {}
+
+        fun result(): Array<E> = ArrayUtils.resizeInPlace(r, ri)
+    }
+
+    class XorMerge<E : Comparable<E>>(val a: Array<E>, val b: Array<E>) : BinaryMerge() {
+        var ri: Int = 0
+        val r = ArrayUtils.newArray(a.size + b.size, a)
+
+        init {
+            merge0(0, a.size, 0, b.size)
+        }
+
+        override fun compare(ai: Int, bi: Int) = a[ai].compareTo(b[bi])
+
+        override fun collision(ai: Int, bi: Int) {}
+
+        override fun fromA(a0: Int, a1: Int, bi: Int) {
+            System.arraycopy(a, a0, r, ri, a1 - a0)
+            ri += a1 - a0
+        }
+        override fun fromB(ai: Int, b0: Int, b1: Int) {
+            System.arraycopy(b, b0, r, ri, b1 - b0)
+            ri += b1 - b0
+        }
 
         fun result(): Array<E> = ArrayUtils.resizeInPlace(r, ri)
     }
