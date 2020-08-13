b70f0e36d1f45eab9065c5b48586acfdf52c08e0
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 07:52:40 +0200

    Added sortAndRemoveDuplicatesInPlace.

--- src/main/kotlin/com/mthaler/arrayset/ArrayUtils.kt
+++ src/main/kotlin/com/mthaler/arrayset/ArrayUtils.kt
@@ -2,6 +2,10 @@ package com.mthaler.arrayset
 
 import kotlin.math.min
 
+fun <T>newArray(n: Int, prototype: Array<T>): Array<T> {
+    return java.lang.reflect.Array.newInstance(prototype.javaClass.componentType, n) as Array<T>
+}
+
 fun <T>resizeInPlace(a: Array<T>, n: Int): Array<T> {
     if (a.size == n) {
         return a
@@ -12,6 +16,20 @@ fun <T>resizeInPlace(a: Array<T>, n: Int): Array<T> {
     }
 }
 
-fun <T>newArray(n: Int, prototype: Array<T>): Array<T> {
-    return java.lang.reflect.Array.newInstance(prototype.javaClass.componentType, n) as Array<T>
+fun <T : Comparable<T>>sortAndRemoveDuplicatesInPlace(a: Array<T>): Array<T> {
+    if(a.size <= 1)
+        return a
+    else {
+        Sorting.sort(a)
+        var j = 1
+        var i = 1
+        while (i < a.size) {
+            if (a[j-1].compareTo(a[i]) == 0) {
+                a[j] = a[i]
+                j += 1
+            }
+            i += 1
+        }
+        return resizeInPlace(a, j)
+    }
 }
