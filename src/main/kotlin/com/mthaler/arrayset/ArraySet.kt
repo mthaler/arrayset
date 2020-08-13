3732025968a1e8cee571f94bbdca7b033621ad47
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 12:23:49 +0200

    Optimized ArraySet.containsAll.

--- src/main/kotlin/com/mthaler/arrayset/ArraySet.kt
+++ src/main/kotlin/com/mthaler/arrayset/ArraySet.kt
@@ -16,14 +16,30 @@ open class ArraySet<E : Comparable<E>>(a: Array<E>) : AbstractSet<E>() {
     override fun contains(element: E): Boolean {
         return Searching.search(elements, 0, elements.size, element) >= 0
     }
-
-    // todo: optimize
-    override fun containsAll(elements: Collection<E>): Boolean {
-        for (element in elements) {
-            if (Searching.search(this.elements, 0, elements.size, element) < 0)
+    
+    override fun containsAll(other: Collection<E>): Boolean {
+        if (other is ArraySet) {
+            if (isEmpty() && other.isEmpty()) {
+                return true
+            }
+            if (size != other.size) {
                 return false
+            }
+            var i = 0
+            while (i < this.elements.size) {
+                if (other.elements[i] != this.elements[i]) {
+                    return false
+                }
+                i++
+            }
+            return true
+        } else {
+            for (element in elements) {
+                if (Searching.search(this.elements, 0, elements.size, element) < 0)
+                    return false
+            }
+            return true
         }
-        return true
     }
 
     override fun isEmpty(): Boolean {
