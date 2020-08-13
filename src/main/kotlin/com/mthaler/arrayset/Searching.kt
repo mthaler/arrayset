4d51f43ec668de17cd75ede90c0b9f75867355b2
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 08:31:03 +0200

    Added Searching object.

---
+++ src/main/kotlin/com/mthaler/arrayset/Searching.kt
@@ -0,0 +1,17 @@
+package com.mthaler.arrayset
+
+object Searching {
+
+    fun <T : Comparable<T>>search(a: Array<T>, from: Int, until: Int, item: T): Int {
+        var first = from
+        var last = until - 1
+        while (first <= last) {
+            val middle = (first + last) ushr 1
+            val compare = a[middle].compareTo(item)
+            if (compare < 0) first = middle + 1
+            else if (compare > 0) last = middle - 1
+            else return middle
+        }
+        return -first - 1
+    }
+}
