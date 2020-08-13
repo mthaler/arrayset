227bd1bdc6e6ace278968b69309674929675ce81
Michael Thaler <michael.thaler@dlr.de>
2020-08-13 09:42:07 +0200

    Added SetUtils.UnionMerge.

--- src/main/kotlin/com/mthaler/arrayset/SetUtils.kt
+++ src/main/kotlin/com/mthaler/arrayset/SetUtils.kt
@@ -2,4 +2,31 @@ package com.mthaler.arrayset
 
 object SetUtils {
 
+    class UnionMerge<E : Comparable<E>>(val a: Array<E>, val b: Array<E>) : BinaryMerge() {
+        val r = ArrayUtils.newArray(a.size + b.size, a)
+        var ri: Int = 0
+
+        init {
+            merge0(0, a.size, 0, b.size)
+        }
+
+        override fun compare(ai: Int, bi: Int) = a[ai].compareTo(b[bi])
+
+        override fun collision(ai: Int, bi: Int) {
+            System.arraycopy(a, ai, r, ri, 1)
+            ri += 1
+        }
+
+        override fun fromA(a0: Int, a1: Int, bi: Int) {
+            System.arraycopy(a, a0, r, ri, a1 - a0)
+            ri += a1 - a0
+        }
+
+        override fun fromB(ai: Int, b0: Int, b1: Int) {
+            System.arraycopy(b, b0, r, ri, b1 - b0)
+            ri += b1 - b0
+        }
+
+        fun result(): Array<E> = ArrayUtils.resizeInPlace(r, ri)
+    }
 }
