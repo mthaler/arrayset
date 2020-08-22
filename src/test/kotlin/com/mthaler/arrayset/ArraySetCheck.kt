f62e2cfb13357cfd357d7be3d5ffa8e61a817009
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-08-22 18:11:52 +0200

    Updated ArraySetCheck.

--- src/test/kotlin/com/mthaler/arrayset/ArraySetCheck.kt
+++ src/test/kotlin/com/mthaler/arrayset/ArraySetCheck.kt
@@ -6,14 +6,20 @@ import io.kotest.property.forAll
 class ArraySetCheck: StringSpec({
 
     "union" {
-        forAll<List<Int>, List<Int>> { a, b ->
+        forAll<Set<Int>, Set<Int>> { a, b ->
             val s1 = ArraySet.of(a)
             val s2 = ArraySet.of(b)
             val s = s1.union(s2)
-            val expected = HashSet<Int>()
-            expected.addAll(a)
-            expected.addAll(b)
-            s == expected
+            s == a.union(b)
+        }
+    }
+
+    "intersection" {
+        forAll<Set<Int>, Set<Int>> { a, b ->
+            val s1 = ArraySet.of(a)
+            val s2 = ArraySet.of(b)
+            val s = s1.intersect(s2)
+            s == a.intersect(b)
         }
     }
 })
